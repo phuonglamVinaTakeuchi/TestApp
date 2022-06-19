7b65b67b018d45d9e950806d2419c9f2a5d74bdb
lam.nguyen <lam.nguyen@eastgate-software.com>
2022-06-19 19:54:47 +0700

    Add project files.

---
+++ TestApp/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿
+
+using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TestApp
+{
+  public class MainWindowViewModel : BindableBase
+  {
+    public Setting ProgramSetting { get; set; }
+
+    public ICommand OnCloseCommand { get; set; }
+
+    public MainWindowViewModel()
+    {
+      ProgramSetting = GetSetting();
+      ProgramSetting.IsModified = false;
+
+      OnCloseCommand = new DelegateCommand(SaveSetting);
+    }
+    private Setting GetSetting()
+    {
+      //using (var streamReader = new StreamReader("Setting/Setting.csv"))
+      //{
+      //  var csvHelper = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+      //  var settings = csvHelper.GetRecords<Setting>();
+      //  return settings.FirstOrDefault();
+      //}
+
+
+      using (StreamReader r = new StreamReader("Setting/Setting.json"))
+      {
+        string json = r.ReadToEnd();
+        var setting = JsonConvert.DeserializeObject<Setting>(json);
+        return setting;
+      }
+
+    }
+
+    private void SaveSetting()
+    {
+      //var records = new List<Setting>
+      //{
+      //    ProgramSetting
+      //};
+
+      //using (var writer = new StreamWriter("Setting/Setting.csv"))
+      //using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+      //{
+      //  csv.WriteRecords(records);
+      //}
+
+      if (MessageBox.Show("May co muon save ko", "Ask Save", MessageBoxButton.YesNo) == MessageBoxResult.No)
+        return;
+
+      using (StreamWriter file = File.CreateText("Setting/Setting.json"))
+      {
+        JsonSerializer serializer = new JsonSerializer();
+        //serialize object directly into file stream
+        serializer.Serialize(file,ProgramSetting);
+        ProgramSetting.IsModified = false;
+        MessageBox.Show("Saved!");
+      }
+    }
+  }
+}
---
+++ TestApp/Setting.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+  public class Setting : BindableBase
+  {
+    private double _width;
+    public double Width { get=>_width; set
+      {
+        if (value == _width)
+          return;
+        _width = value;
+        IsModified = true;
+        RaisePropertyChanged();
+      }
+    }
+    private bool _isModified;
+    public bool IsModified { get=>_isModified; set=>SetProperty(ref _isModified,value); }
+  }
+}
